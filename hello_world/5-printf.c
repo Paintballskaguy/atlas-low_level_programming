b84fa777c3791baf015d1abfa731bdf87692a6ba
Paintballskaguy <Johnstournament@gmail.com>
2024-05-16 18:42:39 +0000

    task 5; created c script to printf phrase

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main: script to print with proper grammar, but the outcome is a piece of art.
+ * return: void. no return neccessary.
+*/
+int main (void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
