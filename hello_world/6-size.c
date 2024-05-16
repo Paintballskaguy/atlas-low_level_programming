a23e9b49274d59edcb4e3a56d020443c1bfe005f
Paintballskaguy <Johnstournament@gmail.com>
2024-05-16 20:51:29 +0000

    task6 created script to printf byte sizes

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main: script to print various types.
+ * Return: return should be void because no return
+*/
+int main (void)
+{
+	printf ("Size of a char: 1 byte(s)\
+Size of an int: 4 byte(s)\
+Size of a long int: 4 byte(s)\
+Size of a long long int: 8 byte(s)\
+Size of a float: 4 byte(s)");
+	return (0);
+}
