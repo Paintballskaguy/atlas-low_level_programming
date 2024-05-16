ff5ef30fb5cc2b87d28519cf93d61d74015c47a2
Paintballskaguy <Johnstournament@gmail.com>
2024-05-16 18:47:45 +0000

    corrected betty issues

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,8 +3,9 @@
  * main: script to print with proper grammar, but the outcome is a piece of art.
  * return: void. no return neccessary.
 */
-int main (void)
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art, ");
-		return (0);
+printf ("with proper grammar, but the outcome is a piece of art,")
+;
+		return(0);
 }
