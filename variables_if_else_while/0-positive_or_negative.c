0eea04157c84f19f45bbd3a5804dbb15e470e7d6
Paintballskaguy <Johnstournament@gmail.com>
2024-05-18 18:47:19 +0000

    task 0 - changed { locations and changed to puts

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2; 
-	{
-	if (n > );
-		printf ("is positive"\n);
-	else if (n == 0);
-		printf ("is zero"\n);
-	else if (n < 0);
-		printf ("is negative"\n);
+	if(n > 0); {
+		puts ("is positive"\n); }
+	else if(n == 0); {
+		puts ("is zero"\n); }
+	else if(n < 0); {
+		puts ("is negative"\n); 
 	}
 	return (0);
 }
