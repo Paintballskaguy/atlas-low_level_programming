388f4a7ad86c2261dbb85bc2b74ec30cfd6b7d88
Paintballskaguy <Johnstournament@gmail.com>
2024-05-18 18:51:40 +0000

    reverted back to printf

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,11 +10,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2; 
 	if(n > 0) {
-		puts ("is positive"); }
+		printf ("is positive"); }
 	else if(n == 0) {
-		puts ("is zero"); }
+		printf ("is zero"); }
 	else if(n < 0) {
-		puts ("is negative"); 
+		printf ("is negative"); 
 	}
 	return (0);
 }
