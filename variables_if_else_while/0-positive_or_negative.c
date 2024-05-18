11b5fcfdb16ddbd3067418dd940c61548fdaf414
Paintballskaguy <Johnstournament@gmail.com>
2024-05-18 19:16:12 +0000

    issues with betty resolved

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,14 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2; 
-	if(n > 0) {
-		printf ("%d is positive\n", n); }
-	else if(n == 0) {
-		printf ("%d is zero\n", n); }
-	else if(n < 0) {
-		printf ("%d is negative\n", n); 
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\positive", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", m);
 	}
 	return (0);
 }
