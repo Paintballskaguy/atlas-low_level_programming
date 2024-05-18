bd119b6a649d77d01f6fd8e3bbc47112da6594f6
Paintballskaguy <Johnstournament@gmail.com>
2024-05-18 22:55:34 +0000

    task 2 - created script to print out alaphabet in order with putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* main this code will print out the alaphabit in lowercase */
+/* return should be 0*/
+
+int main()
+{
+	for (int A = 'a'; A <= 'z'; A++) 
+{
+	putchar(A);
+}
+	putchar('\n');
+	return(0);
+	}
