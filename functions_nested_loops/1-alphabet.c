f276d9216c37bfcf67a90e95bd2c62801fe96c0a
Paintballskaguy <Johnstournament@gmail.com>
2024-05-19 17:15:20 +0000

    task 1 created void alphabet script

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/*
+ * main - checks the code
+ * return - always 0
+*/
+void print_alphabet(void);
+{
+	char x = 'a';
+
+	while(x <= 'z')
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
