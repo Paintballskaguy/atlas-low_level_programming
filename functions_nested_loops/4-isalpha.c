46370dca4fa05ee290ff9dea8b8bc9edd92f54bb
Paintballskaguy <Johnstournament@gmail.com>
2024-05-19 20:18:36 +0000

    task 4 created script for the isalpha and has return

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabet characters
+ * @c: the character to check
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
