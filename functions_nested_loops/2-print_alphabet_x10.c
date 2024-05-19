541ec5cdba109188e3bdd94fbb858be786143f1f
Paintballskaguy <Johnstournament@gmail.com>
2024-05-19 18:22:34 +0000

    task 2 created script to print 10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * main script to print out the alphabet lowercase 10x.
+ * return - always 0.
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+
+	for (i = 0; i <= 10; i++);
+	{
+	
+		while (x <= 'z')
+		{
+		_putchar (x);
+		x++;
+		}
+	}
+	_putchar('\n');
+
+}
