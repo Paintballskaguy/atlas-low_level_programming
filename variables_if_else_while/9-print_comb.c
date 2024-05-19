2216e1200c7754ef14dc0a8eac9f806e639195c9
Paintballskaguy <Johnstournament@gmail.com>
2024-05-19 02:43:09 +0000

    task 9 - created script to print 1-9 with , in between

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - script to print all the possible number combinations.
+ * return - successful with a 0 return.
+*/
+int main(void)
+{
+    int x;
+
+    for (x = 0; x < 10; x++)
+    {
+        putchar(x + '0');
+        if (x < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+    putchar('\n');
+    return (0);
+}	
