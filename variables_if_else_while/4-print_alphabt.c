e89ecf582e959bdb187068080f6e7c94605b45ab
Paintballskaguy <Johnstournament@gmail.com>
2024-05-19 00:06:10 +0000

    task4 made script to print out alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main: entry point is printing alphabet in lowercase.
+ * Return: return should be 0 (success)
+ */
+int main (void)
+{
+	char A;
+
+	while (A = 'a', A <= 'z', A++);
+	{
+	putchar (A);
+
+	putchar ('\n');
+
+		return (0);
+	}
+}
+
