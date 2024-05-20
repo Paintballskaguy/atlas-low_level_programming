b0a1c9d76172f5c9cbd4590ad8a8f6c07fa4860b
Paintballskaguy <Johnstournament@gmail.com>
2024-05-20 23:11:11 +0000

    task 9 - script to print out the times_table - very long and lots of nests

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * this script will do multiplcation tables.
+ * times_table will print out the tables with 9 and all the way to 0.
+ * return will be 0.
+ */
+
+void times_table(void)
+{
+	int row;
+	int col;
+	int total;
+
+	for (row = 0; row <=9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			total = row * col;
+
+			if (col = 0)
+			{
+				_putchar(total + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (total < 10)
+				{
+					_putchar(' ');
+					_putchar(total + '0');
+				}
+				else
+				{
+					_putchar((total / 10) + '0');
+					_putchar((total % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
