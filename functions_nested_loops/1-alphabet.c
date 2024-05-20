21835fdd37b7e733ee269faf09eab84b6fd4086b
Paintballskaguy <Johnstournament@gmail.com>
2024-05-20 22:21:07 +0000

    task 6 - script to print _abs value of x

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
-/*
- * main - checks the code
- * return - always 0
+/**
+ * Description: script to print out the alphabet
+ * print_alphabet: Script to print out the alphabet 1 time.
+ * return - always 0 for success.
 */
+
 void print_alphabet(void)
 {
 	char x = 'a';
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs : script that will write a function that computes absolute value.
+ * @x : number to be checked
+ * return: Always 0 for success
+ */
+
+int _abs(int x)
+{
+	if (x <= 0)
+	{
+		_putchar(x);
+	}
+}
