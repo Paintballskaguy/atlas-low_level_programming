2ffa9224e3b5e0f5d43347dd11d8398ad522fb63
Paintballskaguy <Johnstournament@gmail.com>
2024-05-20 22:49:25 +0000

    task 8 - showing the jack bauer clock for 24.

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * Description : script that prints out the minutes and hour to 24 hours.
+ * jack_bauer(void) - this is the prototype for the script.
+ * @hour - hour counter
+ * @minute - minute counter
+ */
+int jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
