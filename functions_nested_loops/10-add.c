c89c7a5aeab455f318a5d10f88dfc15cb4f00bda
Paintballskaguy <Johnstournament@gmail.com>
2024-05-20 23:43:14 +0000

    task 10 created script to add two numbers together

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * this script should add two integers and return the result.
+ * add - this function will add two integers and solve.
+ * @a - first number.
+ * @b - 2nd number.
+ * return - result of the 2 numbers being added together.
+ */
+
+int add(int a, int b)
+{
+	return a + b;
+}
