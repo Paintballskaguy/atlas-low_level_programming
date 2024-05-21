2743c6a8cbe1f786fa2e711ee5601f445f5a7b1a
Paintballskaguy <Johnstournament@gmail.com>
2024-05-21 03:48:01 +0000

    task 10 - betty issues

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * this script should add two integers and return the result.
+ * add - Adds two integers and returns the result.
+ * @a: The first integer.
+ * @b: The second integer.
  *
- * add - this function will add two integers and solve.
- * @a - first number.
- * @b - 2nd number.
- * return - result of the 2 numbers being added together.
+ * Description: This function takes two integers as input and returns their sum.
+ *
+ * Return: The sum of the two integers.
  */
 
 int add(int a, int b)
