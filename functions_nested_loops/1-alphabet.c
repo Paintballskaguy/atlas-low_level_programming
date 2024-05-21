7d6a217a09c7131398e768748c141219067fef13
Paintballskaguy <Johnstournament@gmail.com>
2024-05-21 00:44:26 +0000

    task 1 - betty issues

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * script to print out the alphabet
- * print_alphabet : Script to print out the alphabet 1 time.
- * return - always 0 for success.
+ * print_alphabet - to print out the alphabet 1 time
+ *
+ * Description - script to print out the alphabet
+ *
+ * return - always 0 for success
 */
 
 void print_alphabet(void)
