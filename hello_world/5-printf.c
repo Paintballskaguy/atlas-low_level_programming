45e563c0377cb26a20b98f6936d8864e92c02c38
Paintballskaguy <Johnstournament@gmail.com>
2024-05-21 03:58:13 +0000

    task 5 - betty issues

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * main: script to print with proper grammar,
- * but the outcome is a piece of art.
- * return: void. no return neccessary.
-*/
+ * main - Prints a specific message with proper grammar.
+ *
+ * Description: This script prints the message "with proper grammar,
+ * but the outcome is a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
