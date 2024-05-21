2543c5e0899edba8523613ad87d453c13e364d35
Paintballskaguy <Johnstournament@gmail.com>
2024-05-21 00:38:03 +0000

    task 11 - updated script issues

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,25 +9,25 @@
 
 void print_to_98(int n)
 {
-	int i, j;
+	int a, x;
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (a = n; a <= 98; a++)
 		{
-			if (i != 98)
-				printf("%d, ", i);
-			else if (i == 98)
-				printf("%d\n", i);
+			if (a != 98)
+				printf("%d, ", a);
+			else if (a == 98)
+				printf("%d\n", a);
 		}
 	} else if (n >= 98)
 	{
-		for (j = n; j >= 98; j--)
+		for (x = n; x >= 98; x--)
 		{
-			if (j != 98)
-				printf("%d, ", j);
-			else if (j == 98)
-				printf("%d\n", j);
+			if (x != 98)
+				printf("%d, ", x);
+			else if (x == 98)
+				printf("%d\n", x);
 		}
 	}
 }
