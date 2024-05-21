f704c88d5abd6cfbaf951b4a878e22dd8ee4efa5
Paintballskaguy <Johnstournament@gmail.com>
2024-05-21 04:14:21 +0000

    task 3 - betty issues

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 /**
-* main - this code will print out the alaphabit in lowercase and uppercase
+* main - this code will print out the alaphabit in
+* lowercase and uppercase
+*
 * Return should be 0 and print out the alphabet 2x.
 */
 
