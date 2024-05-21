b1168672da21a77e8a3961c20694c638b3af7817
Paintballskaguy <Johnstournament@gmail.com>
2024-05-21 04:21:19 +0000

    task 4 - betty issues

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
- /**
- * main - entry point to script
- *
- * Description - Script to print the alphabet in lowercase
- *
- * Return: return should be 0 (success)
- */
+
+/**
+* main - entry point to script
+*
+* Description - Script to print the alphabet in lowercase
+*
+* Return: return should be 0 (success)
+*/
 
 int main(void)
 {
