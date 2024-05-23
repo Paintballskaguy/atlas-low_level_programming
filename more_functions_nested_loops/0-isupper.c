27103695891a71afb1dc6fa4e838c90aea77c517
Paintballskaguy <Johnstournament@gmail.com>
2024-05-23 23:07:16 +0000

    betty issue

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - This should check for an uppercase letter in the int.
  * @c : int to be checked
- * return: 1 if capital letter and 0 if other.
+ * Return: 1 if capital letter and 0 if other.
  *
  */
 
