fb81c188ea1d74d61e60b590353cbc72f57037d5
Paintballskaguy <Johnstournament@gmail.com>
2024-05-23 23:05:06 +0000

    task 0 - added ; after return

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - This should check for an uppercase letter in the int.
  * @c : int to be checked
- * 
+ * return: 1 if capital letter and 0 if other.
  *
  */
 
 int _isupper(int c)
 {
 
-	return (c >= 'A' && c >= 'Z')
+	return (c >= 'A' && c >= 'Z');
 }
 
