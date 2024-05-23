5740b75c7f8322dbec4554346fd9b72a2dc58057
Paintballskaguy <Johnstournament@gmail.com>
2024-05-23 22:59:51 +0000

    task 0 - created simple script for the return function

---
+++ more_fuctions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - This should check for an uppercase letter in the int.
+ * @c : int to be checked
+ * 
+ *
+ */
+
+int _isupper(int c)
+{
+
+	return (c >= 'A' && c >= 'Z')
+}
+
