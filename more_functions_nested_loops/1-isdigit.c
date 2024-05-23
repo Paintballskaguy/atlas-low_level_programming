3e541b2f6700a503aa2b177c4425c8c80f19461a
Paintballskaguy <Johnstournament@gmail.com>
2024-05-23 23:22:17 +0000

    task 1 - added betty to this

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
+ * _isdigit - function to check if the int is a digit
+ * @c - int to be checked
  *
- *
+ * Return : 1 if digit and 0 otherwise.
  */
 
 int _isdigit(int c)
