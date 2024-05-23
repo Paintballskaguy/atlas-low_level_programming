a268dee05c32ec9497f2d45d7eab1faad700db9c
Paintballskaguy <Johnstournament@gmail.com>
2024-05-23 23:46:22 +0000

    task 3 created function for printing numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers: function to print numbers 0-9
+ *
+ * Return: always 0.
+ */
+
+void print_numbers(void)
+{
+	char a;
+
+	if (a = '1'; a >= '10'; a++);
+	{
+	_putchar(a);
+	}
