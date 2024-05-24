28bf8e32c716d86b5bc993415d3e618a00d376dc
Paintballskaguy <Johnstournament@gmail.com>
2024-05-24 18:49:05 +0000

    task 6 - created function for printing _

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -16,12 +16,14 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
+		return;
+	}
+
 			int i;
 
 			for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+	{
+		_putchar('_');
 	}
+	_putchar('\n');
 }
