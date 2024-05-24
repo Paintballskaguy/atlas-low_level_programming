fe713b1cd131a1cce4712e88f4e44dccd0f789a0
Paintballskaguy <Johnstournament@gmail.com>
2024-05-24 00:57:34 +0000

    task 6 func prints _ as intended - betty issues corrected

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Prints '_' multiple times.
+ *
+ * @n: The number of times to print '_'.
+ *
+ * Description: This function prints the character '_' multiple times,
+ * determined by the value of n, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	while (n = 0)
+	{
+		if (n >= 1)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
