470b8ddc9c09e5fe856707f1490642deebea7a8b
Paintballskaguy <Johnstournament@gmail.com>
2024-05-24 00:05:01 +0000

    task 3, corrected loop and added unistd.h library

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * print_numbers - function to print numbers 0-9
@@ -12,7 +13,7 @@ void print_numbers(void)
 {
 	int a;
 
-	for (a = 0; a > 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		_putchar(a + '0');
 	}
