907516d247c727e423c8e51b759252993802f8aa
Paintballskaguy <Johnstournament@gmail.com>
2024-05-24 20:29:15 +0000

    task 7 function to make the diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - this will draw a diagnonal line with _.
+ *
+ * @n - int to be checked
+ *
+ * Description: function that draws a diagnonal line with '_'.
+ *
+ * Return: '_' unless n is less than or equal to 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (x = 0; x < i; x++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	_putchar('\n');
+	}
+}	
