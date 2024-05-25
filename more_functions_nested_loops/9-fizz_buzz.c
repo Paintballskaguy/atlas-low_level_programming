52038ba21af4a7ff2554c4643de8d6deb8bb12f9
Paintballskaguy <Johnstournament@gmail.com>
2024-05-25 03:53:02 +0000

    task 9 created FizzBuzz with standard libary

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function to print FizzBuzz if the int is a multiple
+ * of 3 and 5.
+ *
+ * @pie: int to be checked
+ *
+ * Return: Return FizzBuzz if a number of 3 & 5.
+ * Return Fizz if by 3. Return Buzz if by 5.
+ *
+ */
+
+int main(void)
+{
+	int pie;
+
+	for (pie = 0; pie <= 100; pie++)
+	{
+		if (pie % 3 == '0' && pie % 5 == '0')
+			printf('FizzBuzz');
+		else if (pie % 3 == '0')
+		{
+			printf('Fizz');
+		}
+		else if (pie % 5 == '0')
+		{
+			printf('Buzz');
+		}
+		printf('\n');
+	}
+}
