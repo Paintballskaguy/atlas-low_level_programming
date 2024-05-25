20dfd36ee364f85926c3abb26a7b464a5f1c999a
Paintballskaguy <Johnstournament@gmail.com>
2024-05-25 23:00:58 +0000

    task 0- simple func to reset n to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 
 /**
+ * reset_to_98 - Resets the value of an integer to 98.
  *
+ * @n: Pointer to the integer to be reset.
  *
+ * Description: This function takes a pointer to an integer and
+ * updates the value it points to 98.
+ *
+ * Return: void
  */
 
 void reset_to_98(int *n)
