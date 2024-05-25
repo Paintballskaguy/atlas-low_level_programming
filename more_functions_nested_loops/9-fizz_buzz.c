1e518adbaeecc96f8798af8615c407d8f49688bc
Paintballskaguy <Johnstournament@gmail.com>
2024-05-25 04:15:28 +0000

    task 9 - corrected FIZZBUZZ issues, betty issues

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,20 +18,27 @@ int main(void)
 
 	for (pie = 0; pie <= 100; pie++)
 	{
-		if (pie % 3 == '0' && pie % 5 == '0')
-		{	
-			printf ("FizzBuzz");
-
-		else if (pie % 3 == '0')
+		if (pie % 3 == 0 && pie % 5 == 0)
+		{
+		printf ("FizzBuzz");
+		}
+		else if (pie % 3 == 0)
 		{
 			printf ("Fizz");
 		}
-		else if (pie % 5 == '0')
+		else if (pie % 5 == 0)
 		{
 			printf ("Buzz");
 		}
-		printf ("\n");
+		else
+		{
+			printf ("%d", pie);
+		}
+		if (pie < 100)
+		{
+		printf (" ");
 		}
 	}
+	printf ("\n");
 	return (0);
 }
