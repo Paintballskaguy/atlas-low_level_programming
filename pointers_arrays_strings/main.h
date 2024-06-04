2ce6e4c4bce1efd573339baab8671b64bd712cb2
Paintballskaguy <Johnstournament@gmail.com>
2024-06-04 03:51:11 +0000

    task 6 - prints a chessboard, which is cool

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 
