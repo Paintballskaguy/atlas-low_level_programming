66e9d99d94242ed12bea30080bc2b00097fe42d0
Paintballskaguy <Johnstournament@gmail.com>
2024-06-04 03:25:03 +0000

    task 4 - removed != NULL and put != 0

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -3,6 +3,35 @@
 #include <stdio.h>
 
 /**
+ * _strchr - Locates the first occurrence of the character c
+ *           in the string s.
+ * @s: Pointer to the string to be searched.
+ * @c: The character to locate.
+ *
+ * Return: A pointer to the first occurrence of the character c
+ *         in the string s, or NULL if the character is not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+		
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
+
+
+/**
  * _strpbrk - locates the first occurrence in the string s
  * of any of the bytes in the string accept.
  * @s: the string to be searched
