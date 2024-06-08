64c7c91e6b5a43398f58b822413f05bc005714bf
Paintballskaguy <Johnstournament@gmail.com>
2024-06-07 21:04:38 -0500

    task 3 - factorial. func to print out the factorial approach.

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - function to return the factorial of a given number.
+ * @n: int to be calculated
+ * 
+ * Return: void.
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+	return(1);
+	else
+		return (n * factorial (x - 1));
+}
