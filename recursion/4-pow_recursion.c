20f380b15c7a86b3ae403ea06dd10efadf01ec9a
Paintballskaguy <Johnstournament@gmail.com>
2024-06-08 13:18:01 -0500

    task 4 - corrected last return

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,11 +11,14 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y == '\0')
-	return (1);
+	if (y == 0)
+	{
+		return (1);
+	}
 
-	if (y < '\0')
-		return(_pow_recursion(x, y + 1) / x);
-
-	return (_pow_recursion(x, y - 1) * x);
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
