72fa6b76a2caf8ad275f6c2cdea8db75b80e61d7
Paintballskaguy <Johnstournament@gmail.com>
2024-06-08 13:51:21 -0500

    task 0 - betty

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,17 +5,17 @@
  * _puts_recursion - prints a string and
  * starts a new line.
  * @s: int to be printed
- * 
+ *
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
