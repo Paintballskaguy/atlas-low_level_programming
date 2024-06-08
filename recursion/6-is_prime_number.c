c08151e144aad73f076e9b176ca5656f34e330f9
Paintballskaguy <Johnstournament@gmail.com>
2024-06-08 13:32:44 -0500

    task 6 - formating issues

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -27,8 +27,9 @@ int check_prime(int n, int i)
 {
     if (i * i > n)
         return (1);
-    if (n % i == 0)
-        return (0);
+		
+		if (n % i == 0)
+        	return (0);
 
 	return (check_prime(n, i + 1));
 }
