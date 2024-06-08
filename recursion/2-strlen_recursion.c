89fa51fafb5c6472bc03c2385738c7c334245253
Paintballskaguy <Johnstournament@gmail.com>
2024-06-07 20:54:58 -0500

    task 2 - updated main.h: used recursion to count string length

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - 
+ * @s: 
+ * 
+ * Return: void.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if _strlen_recursion == '\0';
+	{
+	return;
+	}
+return(1 + _strlen_recursion(s + 1;);
+}
