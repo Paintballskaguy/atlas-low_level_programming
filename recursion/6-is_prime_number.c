23f8b61d69a92f0548cbc8e47b897620590ca1cb
Paintballskaguy <Johnstournament@gmail.com>
2024-06-08 13:29:49 -0500

    task 6 - formating issues...

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -6,12 +6,13 @@
  *
  * Return: 1 if n is a prime number, 0 otherwise
  */
+
 int is_prime_number(int n)
 {
     if (n <= 1)
-        return 0;  // Return 0 for numbers less than or equal to 1
+        return (0);
 
-    return check_prime(n, 2);  // Start checking from the smallest prime number, which is 2
+    return (check_prime(n, 2));
 }
 
 /**
@@ -21,12 +22,13 @@ int is_prime_number(int n)
  *
  * Return: 1 if n is a prime number, 0 otherwise
  */
+
 int check_prime(int n, int i)
 {
     if (i * i > n)
-        return 1;  // No divisors found, n is prime
+        return (1);
     if (n % i == 0)
-        return 0;  // Divisor found, n is not prime
+        return (0);
 
-	return check_prime(n, i + 1);  // Check the next possible divisor
+	return (check_prime(n, i + 1));
 }
