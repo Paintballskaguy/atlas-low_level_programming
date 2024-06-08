200dba0a954aaab20c5047adc4fad2c87d70a8ec
Paintballskaguy <Johnstournament@gmail.com>
2024-06-08 13:47:30 -0500

    task -6 -bettyy issues

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -13,7 +13,7 @@ int is_prime_number(int n)
 	if (n <= 1)
 	return (0);
 
-    return (check_prime(n, 2));
+return (check_prime(n, 2));
 }
 
 /**
@@ -27,8 +27,8 @@ int is_prime_number(int n)
 int check_prime(int n, int i)
 {
 	if (i * i > n)
-        return (1);
-		
+	return (1);
+	
 	if (n % i == 0)
 		return (0);
 
