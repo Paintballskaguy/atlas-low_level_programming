07075c343e1945db30c223c8a6c7f2012b7f3fc9
Paintballskaguy <Johnstournament@gmail.com>
2024-06-08 13:12:18 -0500

    task 3 - betty issues

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,16 +3,16 @@
 /**
  * factorial - function to return the factorial of a given number.
  * @n: int to be calculated
- * 
+ *
  * Return: the factorial of n, or -1 if less than 0.
  */
 
 int factorial(int n)
 {
 	if (n < 0)
-		return(-1);
+		return (-1);
 	if (n == 0)
-		return(1);
+		return (1);
 
 	return (n * factorial(n - 1));
 }
