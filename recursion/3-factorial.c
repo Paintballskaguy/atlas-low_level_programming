2c3e106ab0aed4fd8b49d6e63b0c491604e3303e
Paintballskaguy <Johnstournament@gmail.com>
2024-06-07 21:21:30 -0500

    task 3 - factorial correction. had to add another if statement

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,13 +4,15 @@
  * factorial - function to return the factorial of a given number.
  * @n: int to be calculated
  * 
- * Return: void.
+ * Return: the factorial of n, or -1 if less than 0.
  */
 
 int factorial(int n)
 {
-	if (n == 1)
-	return(1);
-	else
-		return (n * factorial (x - 1));
+	if (n < 0)
+		return(-1);
+	if (n == 0)
+		return(1);
+
+	return (n * factorial(n - 1));
 }
