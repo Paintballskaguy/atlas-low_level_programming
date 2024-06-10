93d54f98583b1bf31f2c3559653b72f60024e179
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 16:36:46 -0500

    task 2 - created a test func to show all arguments.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point for loop
+ * @argc: count of arguements
+ * @argv: vector or array of arguments.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d:%s", i, argv[i]);
+	}
+	return (0);
+}
