b5c8c91d97442bc78c6758717593a03e6e7d4757
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 16:20:54 -0500

    task 1 - declared i.

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,12 +6,15 @@
  * main - entry point
  * @argc: gives the amount of arguements.
  * @argv: gives the array of arguements
+ * @i: int to be counted.
  *
  * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
+	int i;
+
 	if (i = 0; i < argc; i++)
 	{
 		(void)argv;
