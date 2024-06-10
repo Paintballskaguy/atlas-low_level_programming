2910df628de0436432468421d30f8097da90cf09
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 00:25:26 -0500

    task 0 - corrected some coding in the functions.

--- static_libraries/_strcpy.c
+++ static_libraries/_strcpy.c
@@ -1,8 +1,23 @@
 #include "main.h"
+
 /**
- * _strcpy
+ * _strcpy - copies the string pointed to by src, including the
+ * terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: the destination buffer
+ * @src: the source string
+ *
+ * Return: pointer to dest
  */
-char *_strcpy(char *dest, char *src, int n)
+
+char *_strcpy(char *dest, char *src)
 {
-	
+    char *orig = dest;
+    
+    while (*src)
+    {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+    
+    return (orig);
 }
--- static_libraries/_strncat.c
+++ static_libraries/_strncat.c
@@ -1,8 +1,31 @@
 #include "main.h"
+#include <string.h>
+
 /**
- * _strncat
+ * _strncat - concatenates two strings using at most n bytes from src.
+ * @dest: destination string.
+ * @src: source string.
+ * @n: number of bytes to use from src.
+ * 
+ * Return: pointer to the resulting string dest.
  */
-char *strcat(char *dest, char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
-	
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
