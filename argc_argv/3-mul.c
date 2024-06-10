ff62c849c448653421568102e84466a2513a6d3b
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 16:53:16 -0500

    task 3 - i had this coded from the first task - saved and pulled to try.

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: gives the amount of arguements.
+ * @argv: gives the array of arguements
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int result = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		int x = strtol(argv[i], NULL, 10);
+		result = (result * x);
+		printf("%d\n", result);
+	}
+	return (0);
+}
