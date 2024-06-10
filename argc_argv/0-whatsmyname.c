dcf357cc5eb1b97899d8305458c8a585efff2403
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 15:17:29 -0500

    task 0 - created read me, main.h, and copied some functions over from static library.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * argc - gives the amount of arguements.
+ * @argv: gives the array of arguements
+ * 
+ * Return: the result of the arugments.
+ */
+
+
---
+++ argc_argv/_puts.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts
+ */
+void _puts(char *s)
+{
+	printf("%s", s);
+}
