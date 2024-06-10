2950953cde58e3fe618c73705417dd4c6ce0a698
Paintballskaguy <Johnstournament@gmail.com>
2024-06-09 23:38:52 -0500

    corrected _puts.c task 0

--- static_libraries/_puts.c
+++ static_libraries/_puts.c
@@ -1,8 +1,9 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts
  */
 void _puts(char *s)
 {
-	
+	printf("%s", s);
 }
---
+++ static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - prints a quote that I made.
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+	_puts("\"You miss 100% of the shots you don't take.\"\n\t- Wayne Gretzky\n\t- Michael Scott");
+
+	return (0);
+}
