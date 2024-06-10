7e524f180387efa1b8f7aa946fb736f7c697d507
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 16:16:13 -0500

    task 1 - testing simplified code;

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -12,14 +12,6 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int result = 0;
-
-	for (i = 0; i < argc; i++)
-	{
-		int x = strtol(argv[i], NULL, 10);
-		result = (result + x);
-		printf("%d\n", result);
-	}
+	printf("%d\n", argc);
 	return (0);
 }
