fd8f7687e723feda545ad3d7c31ef111efbba612
Paintballskaguy <Johnstournament@gmail.com>
2024-06-09 22:07:41 -0500

    updating the .c files with functions

---
+++ static_libraries/_isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _isalpha
+ */
+int _isalpha(int c)
+{
+	
+}
---
+++ static_libraries/_memcpy.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _memcpy
+ */
+int _memcpy(char *dest, char *src, unsigned int n)
+{
+	
+}
