8a25a6baf7751bbc552b624aac7a6c8c334347a4
Paintballskaguy <Johnstournament@gmail.com>
2024-06-10 17:35:01 -0500

    task 4 - func to print all positive numbers.

---
+++ argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ * @argc: gives the amount of arguements.
+ * @argv: gives the array of arguements
+ * @sum: sum of positive numbers
+ * @i:
+ * @x:
+ * Return: 0 on success and 1 on error.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+	int x;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (x = 0; argv[i][x]!= '\0'; x++)
+		{
+			if (!isdigit(argv[i][x]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		int num = atoi(argv[i]);
+		
+			if (num < 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
