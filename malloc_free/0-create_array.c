e267990977df447ce1e3902d10661a11118e373b
Paintballskaguy <Johnstournament@gmail.com>
2024-06-12 12:47:01 -0500

    task 0 - first attempt - with some corrections.

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -9,4 +9,24 @@
  * Return:
  */
 
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = (char*)malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++);
+	{
+		array[i] = c;
+	}
+	return (array);
+}
