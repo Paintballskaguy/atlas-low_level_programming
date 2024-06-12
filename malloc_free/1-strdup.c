99d9c5029c3cdc9c2f676a2a2df72979f9ac0c4c
Paintballskaguy <Johnstournament@gmail.com>
2024-06-12 18:26:03 -0500

    task 1 - betty issues.

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -5,7 +5,7 @@
  * _strdup - returns a pointer to a new string which
  * is a duplicate of the string str.
  * @str: string to be duplicated.
- * 
+ *
  * Return: success will make a pointer to a duplicated string
  * failure will return NULL.
  */
@@ -21,7 +21,8 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (length = 0; str[length] != '\0'; length++)
-;
+; /*calculate the length of the input string*/
+
 	dup = malloc((length + 1) * sizeof(c));
 	if (dup == NULL)
 		return (NULL);
