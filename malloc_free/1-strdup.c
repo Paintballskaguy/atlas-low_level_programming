0a9dd8b2f82de83d1e6352bf4f0d78df599157f6
Paintballskaguy <Johnstournament@gmail.com>
2024-06-12 13:28:38 -0500

    task 1 - func to duplicate a string.

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a new string which
+ * is a duplicate of the string str.
+ * @str: string to be duplicated.
+ * 
+ * Return: success will make a pointer to a duplicated string
+ * failure will return NULL.
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int length;
+	unsigned int i;
+
+	if (str == 0)
+		return (NULL);
+
+	for (length = 0; str[length] != '\0'; length++)
+
+	dup = (char *)malloc((length + 1) * sizeof(char));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+	{
+		dup[i] = str[i];
+	}
+	dup[length] = '\0';
+
+	return (dup);
+}
