be3d5d984c36b4d6c3974eba963280bfe4b6821d
Paintballskaguy <Johnstournament@gmail.com>
2024-06-12 12:23:16 -0500

    task 0 - read me, files and _putchar created.

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - func to create an array of char and int them in order
+ * @size: size of bytes used
+ * @c: char to be examined
+ *
+ * Return:
+ */
+
+
