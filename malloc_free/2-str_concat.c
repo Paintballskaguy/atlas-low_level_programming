f03ac3f76df4a02657fc2d6c3c884f4bd22ed37a
Paintballskaguy <Johnstournament@gmail.com>
2024-06-12 15:43:52 -0500

    task 2 - created file and started process: I think I'm doing too much to make it work.

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - func to concatenates two strings.
+ * @s1: 
+ * @s2:
+ *
+ * Return: 
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	unsigned int length;
+	unsigned int x;
+	char *result;
+
+	if ((s1 == '\0') || (s2 == '\0'))
+		return (NULL);
+
+	char *result = malloc(sizeof(char));
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	
+}
