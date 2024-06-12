9a354ea9bfb4df113500537dde44d4e2e7893e1e
Paintballskaguy <Johnstournament@gmail.com>
2024-06-12 18:04:34 -0500

    task 3- removed sizeof for some reason, i put it back

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
 
-	grid = ((int **)malloc(height * (int *)));
+	grid = ((int **)malloc(height * sizeof(int *)));
 	if (grid == NULL)
 		return (NULL);
 
