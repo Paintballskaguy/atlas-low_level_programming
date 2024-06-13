28fd0c78f244d6e9cba6e9af60aec71a7f15d4f3
Paintballskaguy <Johnstournament@gmail.com>
2024-06-13 17:24:49 -0500

    task 1 - saving in case of loss.

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: A pointer to the newly allocated space in memory containing s1,
+ * followed by the first n bytes of s2, and null terminated.
+ * If the function fails, it should return NULL.
+ * If n is greater or equal to the length of s2 then use the entire string s2.
+ * If NULL is passed for s1 or s2, treat it as an empty string.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	char *c;
+	unsigned int len1 = 0, len2 = 0, i, x;
+
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	}
+	if (s2 != NULL)
+	{
+		for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	}
+	if (n >= len2)
+		n = len2;
+
+		concat = malloc(sizeof(*c) * (len1 + n + 1));
+
+		if (concat == NULL)
+			return (NULL);
+
+}
