7f6c7c76183286eeafb51e5083825d51229493b3
Paintballskaguy <Johnstournament@gmail.com>
2024-06-13 17:33:36 -0500

    task 1 - formatted code in VS

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,20 +23,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 != NULL)
 	{
 		for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
+			;
 	}
 	if (s2 != NULL)
 	{
 		for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+			;
 	}
 	if (n >= len2)
 		n = len2;
 
-		concat = malloc(sizeof(*c) * (len1 + n + 1));
+	concat = malloc(sizeof(*c) * (len1 + n + 1));
 
-		if (concat == NULL)
-			return (NULL);
+	if (concat == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
