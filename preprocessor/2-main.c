10833740951cb6e1b7f6f9888b5bdc7729a525af
Paintballskaguy <Johnstournament@gmail.com>
2024-06-17 20:34:23 -0500

    task 2 - command to print the file the input came from.

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * this will print out the name of the file the command was pulled from
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return(0);
+}
