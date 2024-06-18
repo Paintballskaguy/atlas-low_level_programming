eac9364689c305622e2f043ddb5878c403fb8ba1
Paintballskaguy <Johnstournament@gmail.com>
2024-06-17 20:43:20 -0500

    task 3 - func to get the absolute number.

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - computes the absolute value of a number (X)
+ * @x: int number to be solved
+ */
+
+int main(int x)
+{
+	abs(x)
+	return (x);
+}
--- preprocessor/main.h
+++ preprocessor/main.h
@@ -3,5 +3,6 @@
 
 #define SIZE 1024
 #define PI 3.14159265359
+#define ABS(x)
 
 #endif /*MAIN_H*/
