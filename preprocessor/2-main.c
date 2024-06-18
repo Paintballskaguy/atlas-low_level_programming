dac5079cf0469dcb22c9e4aa389912a644fa792d
Paintballskaguy <Johnstournament@gmail.com>
2024-06-17 20:37:27 -0500

    task 2 - betty issues.

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 /**
- * this will print out the name of the file the command was pulled from
+ * main - prints out the file name where the input is coming from.
+ * 
+ * Return: always 0.
  */
 
 int main(void)
