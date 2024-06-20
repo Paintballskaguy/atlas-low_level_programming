bf173b870bd4d3495b1dd54cf1b25333559b91a2
Paintballskaguy <Johnstournament@gmail.com>
2024-06-19 22:28:02 -0500

    task 0 - main.h, readme and dog.h made.

---
+++ structure_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct representing a dog.
+ * @name: name of the dog (%s)
+ * @age: age of the dog (%f)
+ * @owener: owner of the dog (%s)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
