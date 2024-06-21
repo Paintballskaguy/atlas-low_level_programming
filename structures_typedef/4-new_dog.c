cd85a0e32952558eaf77c10f53ce4d7701b06b3f
Paintballskaguy <johnstournament@gmail.com>
2024-06-21 10:47:44 -0500

    task 4 - updated dog.h with previous func codes for _strlen and _strcpy. updated code to include the _ functions instead of std lib.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,32 +4,6 @@
 #include <string.h>
 
 /**
- * _strdup - Duplicates a string by allocating new memory.
- * @str: The string to duplicate.
- *
- * Return: Pointer to the new string, or NULL if memory allocation fails.
- */
-char *_strdup(const char *str)
-{
-	char *dup;
-	size_t i, len;
-
-	if (str == NULL)
-		return (NULL);
-
-	len = strlen(str);
-	dup = malloc((len + 1) * sizeof(char));
-	if (dup == NULL)
-		return (NULL);
-
-	for (i = 0; i < len; i++)
-		dup[i] = str[i];
-	dup[len] = '\0';
-
-	return (dup);
-}
-
-/**
  * new_dog - func to create a new dog struct and members.
  * @name: name of the member(dog)
  * @age: age of the dog
@@ -46,21 +20,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (snoop_doggy_dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = malloc(_strlen(name) + 1);
 	if (name_copy == NULL)
 	{
 		free(snoop_doggy_dog);
 		return (NULL);
 	}
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = malloc(_strlen(owner) + 1);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(snoop_doggy_dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
-	strcpy(owner_copy, owner);
+	_strcpy(name_copy, name);
+	_strcpy(owner_copy, owner);
 
 	snoop_doggy_dog->name = name_copy;
 	snoop_doggy_dog->age = age;
