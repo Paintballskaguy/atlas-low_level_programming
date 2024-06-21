05ff09f5bdeaface4e2bc3d0ba87e740a7318e0c
Paintballskaguy <Johnstournament@gmail.com>
2024-06-20 22:30:05 -0500

    added dog_t for the nickname for my struct to the define values at the end.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -36,9 +36,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	strcpy(name_copy, name);
 	strcpy(owner_copy, owner);
 
-	new_dog->name = name_copy;
-	new_dog->age = age;
-	new_dog->owner_copy = owner;
+	dog_t new_dog->name = name_copy;
+	dog_t new_dog->age = age;
+	dog_t new_dog->owner_copy = owner;
 
 	return (new_dog);
 }
