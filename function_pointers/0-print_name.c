9a801fd987cc6be32d9d4ed19f386b2c90b951e5
Paintballskaguy <johnstournament@gmail.com>
2024-06-21 16:54:33 -0500

    task 0 - created func to print name.

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include "main.h"
+
+/**
+ * print_name - Prints a name using the given func
+ * @name: name to be printed
+ * @x: func pointer to a function that takes a char *
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char 8))
+{
+	if (name && x)
+	x(name);
+}
