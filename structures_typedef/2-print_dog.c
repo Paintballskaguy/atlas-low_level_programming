c6de06c83f6c499ffd8b1c08e03c1825aa2abb47
Paintballskaguy <Johnstournament@gmail.com>
2024-06-20 21:37:16 -0500

    task 2 - printf of all the members of the struct. updated dog.h with the prototype.

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - func that will print out the members of the struct dog.
+ * @d: int that points to the dog parts.
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else  
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
