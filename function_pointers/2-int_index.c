2fa3e27c8f11f806c062f7c60229d77cb7aa78e2
Paintballskaguy <Johnstournament@gmail.com>
2024-06-23 19:16:19 -0500

    task 2- betty issues

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,7 +6,8 @@
  * @size: Number of elements in the array
  * @cmp: Pointer to the function to be used to compare values
  *
- * Return: Index of the first element for which the cmp function does not return 0
+ * Return: Index of the first element for which
+ * the cmp function does not return 0
  *         If no element matches or size <= 0, return -1
  */
 
