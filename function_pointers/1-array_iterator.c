72d88fedab3ec92e235e3eef5cb3316792e9dc15
Paintballskaguy <Johnstournament@gmail.com>
2024-06-23 19:03:51 -0500

    task 1 - created a function pointer. updated header files

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a parameter
+ *                  on each element of an array.
+ * @array: Array of integers
+ * @size: Size of the array
+ * @action: Pointer to the function to be used
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
