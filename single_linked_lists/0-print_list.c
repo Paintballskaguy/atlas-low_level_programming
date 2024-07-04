bd3cbca7e7272de9a3062b6fbe3b60aaff0e61f5
Paintballskaguy <Johnstournament@gmail.com>
2024-07-03 20:08:31 -0500

    task 0 - created func to print the stuff

--- single_linked_lists/0-print_list.c
+++ single_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+
+/**
+ * print_list - prints all the stuff of a list_t list
+ * @h: pointer to list_t
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
