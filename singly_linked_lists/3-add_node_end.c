5301cb0a3631726fe653bf71b206b5264ecd5f52
Paintballskaguy <Johnstournament@gmail.com>
2024-07-03 21:08:26 -0500

    task 3 - added ; line 18.

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	if (str == NULL)
-		return (NULL)
+		return (NULL);
 
 	dup_str = strdup(str);
 	if (dup_str == NULL)
