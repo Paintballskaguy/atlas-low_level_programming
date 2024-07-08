b1c92089f5eabf4047028670f96af694d0342ed1
Paintballskaguy <johnstournament@gmail.com>
2024-07-08 16:44:15 -0500

    task 10 - created func to delete a node and updated header

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t list
+ * @head: double pointer to the head of the list
+ * @index: index of the node that should be deleted, starting at 0
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *prev;
+	unsigned int i;
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	for (i = 0; temp != NULL && i < index; i++);
+	{
+		prev = temp;
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+		return (-1);
+	
+	prev->next = temp->next;
+	free(temp);
+
+	return (1);
+}
