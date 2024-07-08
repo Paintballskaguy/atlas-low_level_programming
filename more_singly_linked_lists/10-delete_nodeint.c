a23daad2fd59a8f94bc9fe3b57f86c6580316fba
Paintballskaguy <johnstournament@gmail.com>
2024-07-08 16:48:10 -0500

    task 10 - betty issues

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index index of a listint_t list
+ * delete_nodeint_at_index - deletes the node at index
+ * index of a listint_t list
  * @head: double pointer to the head of the list
  * @index: index of the node that should be deleted, starting at 0
  *
@@ -33,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (temp == NULL)
 		return (-1);
-	
+
 	prev->next = temp->next;
 	free(temp);
 
