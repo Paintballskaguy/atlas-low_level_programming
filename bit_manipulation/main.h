83c3580a65f39761324ff5086314f426b489066a
Paintballskaguy <Johnstournament@gmail.com>
2024-07-08 19:19:12 -0500

    task 0 - updated main.h to include the standard libraries

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 
