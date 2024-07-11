86a1afe28e59c68895e40d78fc6c360cc79a7263
Paintballskaguy <Johnstournament@gmail.com>
2024-07-10 20:25:57 -0500

    task 2 - removed a base case for *head

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,9 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (*head == NULL)
-		return (NULL);
-
 	if (new_node == NULL)
 		return (NULL);
 
