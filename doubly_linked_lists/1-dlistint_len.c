00e04428cc3fea57b2db0894ab702a2ded6dd60e
Paintballskaguy <Johnstournament@gmail.com>
2024-07-10 19:39:39 -0500

    task 1 - betty issues

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
- * 
+ * dlistint_len_recursive - Returns the number of elements in a doubly linked list.
+ * @h: Pointer to the head of the list.
  *
+ * Return: The number of elements in the list.
  */
 
 size_t dlistint_len(const dlistint_t *h)
