256f4f25a8d556b7d65d5841712af95be68e6f30
Paintballskaguy <johnstournament@gmail.com>
2024-07-11 12:10:46 -0500

    task 5 - created func to return the node that a script calls for. Updated tester and it's not finding the [5]!!!!

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * @head: pointer to the head of the list.
+ * @index: index of the node, starting from 0.
+ *
+ * Return: pointer to the nth node, or NULL if the node does not exist.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+
+	if (index == '0')
+		return (head);
+
+	return (get_dnodeint_at_index(head->next, index - 1));
+}
--- doubly_linked_lists/test.c
+++ doubly_linked_lists/test.c
@@ -11,16 +11,34 @@
 int main(void)
 {
     dlistint_t *head;
+    dlistint_t *node;
 
     head = NULL;
-    add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
     print_dlistint(head);
+
+    node = get_dnodeint_at_index(head, 5);
+    if (node == NULL)
+    {
+        printf("Error: Node not found at index 5\n");
+    }
+    else if (node->n != 98)
+    {
+        printf("Error: Expected value at index 5 is 98, but got %d\n", node->n);
+    }
+    else
+    {
+        printf("Value at index 5 is: %d\n", node->n);
+    }
+
+    free_dlistint(head);
+    head = NULL;
     return (EXIT_SUCCESS);
 }
