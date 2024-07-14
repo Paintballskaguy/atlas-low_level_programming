86c0c2f40f78afb464788c6b9deb986b3c0bc136
Paintballskaguy <Johnstournament@gmail.com>
2024-07-13 22:14:21 -0500

    task 0 - created files, header, and directory.

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hashtable.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: A pointer to the newly created hash table, or NULL on failure.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if = (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht
+}
