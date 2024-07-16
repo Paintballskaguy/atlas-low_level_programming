c5bd1e298f4f18dd33e16f000d4e16b2b5d5438b
Paintballskaguy <Johnstournament@gmail.com>
2024-07-15 22:05:02 -0500

    task 4 - betty stuff.

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,8 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table you want to look into.
+ * @key: The key you are looking for.
+ *
+ * Return: The value associated with the element, or NULL if key couldn’t be found.
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
