3a8d6bb7317a38aa613e009bf5f6427b955cb434
Paintballskaguy <Johnstournament@gmail.com>
2024-07-15 22:02:32 -0500

    task 4 - func to get a value with a key. hash!

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * 
+ * 
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
